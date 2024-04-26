b58b073c50a42c79cb0ad37f8047b46c60ed1454
hzy-hits <1106796240@qq.com>
2024-04-26 00:22:00 -0400

    convey the eslinter

--- components/theme-toggle-button.js
+++ components/theme-toggle-button.js
@@ -2,31 +2,38 @@ import { AnimatePresence, motion } from 'framer-motion'
 import { IconButton, useColorMode, useColorModeValue } from '@chakra-ui/react'
 import { SunIcon, MoonIcon } from '@chakra-ui/icons'
 import { useRouter } from 'next/router'
+
 const ThemeToggleButton = () => {
-    const { toggleColorMode } = useColorMode()
-    const router = useRouter()
-    if (router.pathname === '/') {
-        return null;
-    }
-    return (
-        <AnimatePresence mode="wait" initial={false}>
-            <motion.div
-                style={{ display: 'inline-block' }}
-                key={useColorModeValue('light', 'dark')}
-                initial={{ y: -20, opacity: 0 }}
-                animate={{ y: 0, opacity: 1 }}
-                exit={{ y: 20, opacity: 0 }}
-                transition={{ duration: 0.2 }}
-            >
-                <IconButton
-                    aria-label="Toggle theme"
-                    colorScheme={useColorModeValue('purple', 'orange')}
-                    icon={useColorModeValue(<MoonIcon />, <SunIcon />)}
-                    onClick={toggleColorMode}
-                ></IconButton>
-            </motion.div>
-        </AnimatePresence>
-    )
+  const { toggleColorMode } = useColorMode()
+  const router = useRouter()
+
+  const colorScheme = useColorModeValue('purple', 'orange')
+  const icon = useColorModeValue(<MoonIcon />, <SunIcon />)
+  const colorModeKey = useColorModeValue('light', 'dark')
+
+  if (router.pathname === '/') {
+    return null
+  }
+
+  return (
+    <AnimatePresence mode="wait" initial={false}>
+      <motion.div
+        style={{ display: 'inline-block' }}
+        key={colorModeKey}
+        initial={{ y: -20, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        exit={{ y: 20, opacity: 0 }}
+        transition={{ duration: 0.2 }}
+      >
+        <IconButton
+          aria-label="Toggle theme"
+          colorScheme={colorScheme}
+          icon={icon}
+          onClick={toggleColorMode}
+        />
+      </motion.div>
+    </AnimatePresence>
+  )
 }
 
 export default ThemeToggleButton
